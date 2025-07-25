82aa8afebbce5e613af022d3c9b75364c239c454
EduardoStarZ <pathver@gmail.com>
2025-07-25 12:00:43 -0300

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use i3ipc::{I3EventListener,
+Subscription,
+event::{Event, inner::WindowChange}};
+
+fn main() {
+    let mut rx : I3EventListener = I3EventListener::connect().unwrap();
+
+    rx.subscribe(&[Subscription::Window]).unwrap();
+    for event in rx.listen() {
+        let w_event = event.unwrap();
+        
+        match w_event {
+            Event::WindowEvent(inner_window_event) => {
+                let info : WindowChange = inner_window_event.change;
+                let mut response : String = String::from("No changes happened");
+                match info {
+                    WindowChange::New => response = String::from("New window opened"),
+                    WindowChange::Close => response = String::from("Window Closed"),
+                    WindowChange::Focus => response = String::from("Window has gained focus"),
+                    WindowChange::FullscreenMode => response = String::from("An window has become fullscreen"),
+                    _ => ()
+                }
+                println!("{response}");
+            },
+            _ => ()
+
+        }
+    }
+}
+
